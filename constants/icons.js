408dabddcbae1929e61b631527b994925d799f1c
Ben Carruth <79821597+S587273@users.noreply.github.com>
2021-10-15 15:44:53 +0930

    constants folder

---
+++ constants/icons.js
@@ -0,0 +1,44 @@
+// Defining icons to be used within App //
+
+const back = require("../assets/icons/back.png");
+const bell = require("../assets/icons/bell.png");
+const bill = require("../assets/icons/bill.png");
+const bitcoin = require("../assets/icons/bitcoin.png");
+const close = require("../assets/icons/close.png");
+const disable_eye = require("../assets/icons/disable_eye.png");
+const down = require("../assets/icons/down.png");
+const eye = require("../assets/icons/eye.png")
+const ethereum = require("../assets/icons/ethereum.png");
+const barcode = require("../assets/icons/barcode.png");
+const info = require("../assets/icons/info.png");
+const dogecoin = require("../assets/icons/dogecoin.png");
+const more = require("../assets/icons/more.png");
+const qr_code = require("../assets/icons/qr_code.png");
+const reload = require("../assets/icons/reload.png");
+const scan = require("../assets/icons/scan.png");
+const send = require("../assets/icons/send.png");
+const stocks = require("../assets/icons/stocks.png");
+const user = require("../assets/icons/user.png");
+
+
+export default {
+    back,
+    bell,
+    bill,
+    bitcoin,
+    close,
+    disable_eye,
+    down,
+    eye,
+    ethereum,
+    barcode,
+    info,
+    dogecoin,
+    more,
+    qr_code,
+    reload,
+    scan,
+    send,
+    stocks,
+    user
+}
---
+++ constants/images.js
@@ -0,0 +1,18 @@
+const smartcoin_logo = require("../assets/images/smartcoin_logo.png");
+const banner = require("../assets/images/banner.png");
+const SpecialPromos = require("../assets/images/promo1.png");
+const focus = require("../assets/images/focus.png");
+
+// Dummy App //
+const aus_flag = require("../assets/images/aus_flag.jpg");
+
+
+export default {
+    smartcoin_logo,
+    banner,
+    SpecialPromos,
+    focus,
+
+    // Dummy flag //
+    aus_flag
+}
